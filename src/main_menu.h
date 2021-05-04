5f5d7b35fcfbda74256036ead35791bc4a4179cc
RMashkalev <rmashkalev@mail.ru>
2021-05-04 22:19:21 +0700

    Create a simple structure

---
+++ src/main_menu.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <locale.h>
+
+typedef struct
+{
+    char description[100];
+    char name[50];
+    char category[25];
+    char priority[4];
+    char status;
+    int day;
+    int month;
+    int year;
+    int progress;
+} task;
+
+void todolist(FILE *file[10]);
+
+int archive();
+
+int bin();
+
+int settings();
+
+void authors();
