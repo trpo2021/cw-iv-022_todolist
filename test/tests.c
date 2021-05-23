dfe8d929099d213080a532b7eaa40374bbdc2306
Arhip <arhip.konyshev@gmail.com>
2021-05-24 04:18:09 +0700

    Add tests for 'find_id'

--- test/tests.c
+++ test/tests.c
@@ -1,3 +1,32 @@
 #include "ctest.h"
-#include <src/todo_list.h>
+#include <todo_list.h>
 
+FILE* file[10]
+            = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+
+CTEST(find_ids, id_1)
+{   
+    file[0] = fopen("test/users/user1.txt", "r+");
+    int result = find_id(file[0]);
+    int expected = 2;
+    ASSERT_EQUAL(expected, result);
+    fclose(file[0]);
+}
+
+CTEST(find_ids, id_2)
+{   
+    file[0] = fopen("test/users/user2.txt", "r+");
+    int result = find_id(file[0]);
+    int expected = 1;
+    ASSERT_EQUAL(expected, result);
+    fclose(file[0]);
+}
+
+CTEST(find_ids, id_3)
+{   
+    file[0] = fopen("test/users/user3.txt", "r+");
+    int result = find_id(file[0]);
+    int expected = 10001;
+    ASSERT_EQUAL(expected, result);
+    fclose(file[0]);
+}
